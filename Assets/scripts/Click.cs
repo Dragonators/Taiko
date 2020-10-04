27aec6f5a84fccd9d91b059d9cc8befa7b826dc1
Dragonators <13391169509@163.com>
2020-10-05 01:19:10 +0800

    ver0.5.0

---
+++ Assets/scripts/Click.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Click : MonoBehaviour
+{
+    public double Centerjudge;
+    public bool notetype;
+    private bool BeginDestroy=false;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        this.GetComponent<RectTransform>().localPosition=new Vector3(20.1f,0.62f,-0.82f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(-12*Time.deltaTime,0,0);
+        if(BeginDestroy)fade();
+    }
+    private void fade() 
+    {
+        this.GetComponent<RectTransform>().localScale=Vector3.Lerp(this.GetComponent<RectTransform>().localScale,new Vector3(0,0,0),Time.deltaTime*10);
+        if(this.GetComponent<RectTransform>().localScale.x<=0.2)
+        {
+            Destroy(this.transform.gameObject);
+        }
+    }
+
+    public void click()
+    {
+        BeginDestroy=true;
+    }
+    public void falseclick()
+    {
+        Destroy(this.transform.gameObject);
+    }
+
+}
--- Assets/scripts/Maincontrol.cs
+++ Assets/scripts/Maincontrol.cs
@@ -11,19 +11,55 @@ public class Maincontrol : MonoBehaviour
     public GameObject Loutdrum;
     public GameObject Rindrum;
     public GameObject Routdrum;
+    public GameObject taikobar;
+    public GameObject Score;
+    public GameObject Combo;
+
     public AudioSource clip;
+    public AudioSource BGM;
+    private double trackStartTime;
+    private double BPM=244;
+    private double nextTick=0.0F;
+    public double Timeline;
+    private double Goodoffset=0.025;
+    public double OKoffset=0.075;
+    private double Timeoffset=1.2;
+    private int combo=0;
+    private int score=0;
+
+    public GameObject noteR;
+    public GameObject noteB;
+    //private ArrayList note=new ArrayList();
+    LinkedList<GameObject> note=new LinkedList<GameObject>();
+    private GameObject k;
     void Start()
     {
-        
+        StartMusic();
     }
-
     // Update is called once per frame
     void Update()
     {
-        input();
+        Timeline=AudioSettings.dspTime-trackStartTime;
+        if(Timeline>=nextTick*60.0/BPM-Timeoffset)
+        {
+            if(Random.Range(0,2)==1)createnoteB(nextTick*60/BPM);else createnoteR(nextTick*60/BPM);
+            nextTick++;
+        }
+        if(note.Count!=0)
+        {
+            if(note.First.Value.GetComponent<Click>().Centerjudge+OKoffset<=Timeline)
+            {
+                note.First.Value.GetComponent<Click>().falseclick();
+                note.RemoveFirst();
+            }
+            judgement();
+        }
+        Pressshow();
         soundeffect();
+        Score.GetComponent<Text>().text=score.ToString();
+        Combo.GetComponent<Text>().text=combo.ToString();
     }
-    private void input()
+    private void Pressshow()
     {
         if(Input.GetKey(KeyCode.Z))Loutdrum.SetActive(true);
         else Loutdrum.SetActive(false);
@@ -34,6 +70,85 @@ public class Maincontrol : MonoBehaviour
         if(Input.GetKey(KeyCode.V))Routdrum.SetActive(true);
         else Routdrum.SetActive(false);
     }
+    private void judgement()
+    {
+        GameObject a=note.First.Value;
+        if(Input.GetKeyDown(KeyCode.X)||Input.GetKeyDown(KeyCode.C))
+        {
+            if(!a.GetComponent<Click>().notetype)
+            {
+                if(a.GetComponent<Click>().Centerjudge-Goodoffset<=Timeline&&a.GetComponent<Click>().Centerjudge+Goodoffset>=Timeline)
+                {
+                    Delnote();
+                    a.GetComponent<Click>().click();
+                    combo++;
+                    score+=300;
+                }
+                else if(a.GetComponent<Click>().Centerjudge-OKoffset<=Timeline&&a.GetComponent<Click>().Centerjudge+OKoffset>=Timeline)
+                {
+                    Delnote();
+                    a.GetComponent<Click>().click();
+                    combo++;
+                    score+=100;
+                }
+                else if(Timeline<a.GetComponent<Click>().Centerjudge-OKoffset&&Timeline>a.GetComponent<Click>().Centerjudge-0.15)
+                {
+                    Delnote();
+                    a.GetComponent<Click>().falseclick();
+                    combo=0;
+                }
+                else if(Timeline>a.GetComponent<Click>().Centerjudge+OKoffset)
+                {
+                    Delnote();
+                    a.GetComponent<Click>().falseclick();
+                    combo=0;
+                }
+                else if(Timeline>=a.GetComponent<Click>().Centerjudge+0.15)
+                {
+
+                }
+            }
+            //else 打错note颜色的情况
+        }
+        if(Input.GetKeyDown(KeyCode.Z)||Input.GetKeyDown(KeyCode.V))
+        {
+            if(a.GetComponent<Click>().notetype)
+            {
+                if(a.GetComponent<Click>().Centerjudge-Goodoffset<=Timeline&&a.GetComponent<Click>().Centerjudge+Goodoffset>=Timeline)
+                {
+                    Delnote();
+                    a.GetComponent<Click>().click();
+                    combo++;
+                    score+=300;
+                }
+                else if(a.GetComponent<Click>().Centerjudge-OKoffset<=Timeline&&a.GetComponent<Click>().Centerjudge+OKoffset>=Timeline)
+                {
+                    Delnote();
+                    a.GetComponent<Click>().click();
+                    combo++;
+                    score+=100;
+                }
+                else if(Timeline<a.GetComponent<Click>().Centerjudge-OKoffset&&Timeline>a.GetComponent<Click>().Centerjudge-0.15)
+                {
+                    Delnote();
+                    a.GetComponent<Click>().falseclick();
+                    combo=0;
+                }
+                else if(Timeline>a.GetComponent<Click>().Centerjudge+OKoffset)
+                {
+                    Delnote();
+                    a.GetComponent<Click>().falseclick();
+                    combo=0;
+                }
+                else if(Timeline>=a.GetComponent<Click>().Centerjudge+0.15)
+                {
+
+                }
+            }
+            //else 打错note颜色的情况
+        }
+
+    }
     private void soundeffect()
     {
         if(Input.GetKeyDown(KeyCode.Z))clip.Play();
@@ -41,4 +156,25 @@ public class Maincontrol : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.C))clip.Play();
         if(Input.GetKeyDown(KeyCode.V))clip.Play();
     }
+    public void StartMusic()
+    {
+        trackStartTime=AudioSettings.dspTime + 3;
+        BGM.PlayScheduled(trackStartTime);
+    }
+    private void createnoteB(double centertime)
+    {
+        k=Instantiate(noteB);
+        k.GetComponent<Click>().Centerjudge=centertime;
+        k.transform.SetParent(taikobar.transform);
+        note.AddLast(k);
+    }
+    private void createnoteR(double centertime)
+    {
+        k=Instantiate(noteR);
+        k.GetComponent<Click>().Centerjudge=centertime;
+        k.transform.SetParent(taikobar.transform);
+        note.AddLast(k);
+    }
+    public void Delnote(){note.RemoveFirst();}
+
 }
